14f5b0af809d947aeda725de41164d736fa782c1
ivantsanev64 <ivan.tsanev.pm@gmail.com>
2025-12-10 19:40:18 +0200

    Initial commit: Gym Management System

---
+++ config.js
@@ -0,0 +1,11 @@
+const FEATURE_FLAGS = {
+  AI_RECOMMENDATIONS: true,
+  FACE_RECOGNITION: false,
+  PAYMENT_GATEWAY: false,
+  DARK_MODE: true
+};
+
+// Usage in code
+if (FEATURE_FLAGS.AI_RECOMMENDATIONS) {
+  showRecommendedClasses();
+}
